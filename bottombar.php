0f5b45fe4d2a5247f557d9758d4680452c52bcb9
J. M. F. Tsang <j.m.f.tsang@cantab.net>
2020-06-18 22:37:50 +0100

    Link to the GitHub repo in the bottom bar

--- bottombar.php
+++ bottombar.php
@@ -1,7 +1,9 @@
 <div class="spacer"></div>
 
 <p id="extlns">
-<a class="extln" href="https://www.facebook.com/QueensGradChoir">Grad Choir on Facebook</a>
+<a class="extln" href="https://www.facebook.com/QueensGradChoir">Facebook</a>
+&bullet;
+<a class="extln" href="https://github.com/jftsang/qgradchoir">GitHub</a>
 </p>
 
 <p id="upd">Updated <?php echo date("jS M Y", getlastmod()); ?></p>
