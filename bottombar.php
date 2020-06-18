90a8b6175d7e7cc664f706a763c443c0d809df42
J. M. F. Tsang <j.m.f.tsang@cantab.net>
2020-06-18 22:32:41 +0100

    Add link to our Facebook page in the bottom bar

--- bottombar.php
+++ bottombar.php
@@ -1,3 +1,7 @@
 <div class="spacer"></div>
 
+<p id="extlns">
+<a class="extln" href="https://www.facebook.com/QueensGradChoir">Grad Choir on Facebook</a>
+</p>
+
 <p id="upd">Updated <?php echo date("jS M Y", getlastmod()); ?></p>
