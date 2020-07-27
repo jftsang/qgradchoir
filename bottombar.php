74c609878cb9e30eba50c296c4fcf12bbcf968e8
J. M. F. Tsang <j.m.f.tsang@cantab.net>
2020-07-27 16:18:36 +0100

    Add a privacy policy

--- bottombar.php
+++ bottombar.php
@@ -1,9 +1,13 @@
 <div class="spacer"></div>
 
 <p id="extlns">
+<a class="extln" href="https://lists.cam.ac.uk/mailman/listinfo/quen-mcr-choir">Mailing list</a>
+&bullet;
 <a class="extln" href="https://www.facebook.com/QueensGradChoir">Facebook</a>
 &bullet;
 <a class="extln" href="https://github.com/jftsang/qgradchoir">GitHub</a>
+&bullet;
+<a href="/privacy.php">Privacy policy</a>
 </p>
 
 <p id="upd">Updated <?php echo date("jS M Y", getlastmod()); ?></p>
---
+++ privacy.php
@@ -0,0 +1,30 @@
+<?php
+require 'header.php';
+?>
+
+<body>
+
+<h1>Queens' Graduate Choir</h1>
+
+<? require_once("topbar.php") ?>
+
+<h2>Privacy policy</h2>
+
+<p>We'll use your email address to keep you updated about our events,
+including our rehearsals, performances, masterclasses and socials.</p>
+
+<p>The membership of the mailing list is visible to all members of the
+mailing list. We do not share individual email addresses outside of the
+list, and only list members have the ability to send to the list.</p>
+
+<p>Emails sent to the public mailing list are archived, and the archives
+are visible to all members of the mailing list.</p>
+
+<p>You are welcome to withdraw from the list at any time. Simply go to
+<a href="https://lists.cam.ac.uk/mailman/listinfo/quen-mcr-choir">our
+mailing list page</a> and follow the instructions, or email the <a
+href="/people.php">committee</a>.</p>
+
+<? require_once("bottombar.php") ?>
+</body>
+</html>
